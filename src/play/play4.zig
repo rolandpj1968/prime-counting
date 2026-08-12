4890f1da59d0e00de6bfd2ef7e5eff33fba770f0
Roland PJ <rolandpj@gmail.com>
2026-08-12 04:39:37 +0200

    unique nodes at ~O(x^0.6) with x p_a^2 pruning

--- src/play/play4.zig
+++ src/play/play4.zig
@@ -7,6 +7,9 @@ const rs = @import("rs");
 
 pub const Counters = struct { n: u64 = 0, x0: u64 = 0, a0: u64 = 0, phi1: u64 = 0, pi: u64 = 0, pi_: u64 = 0 };
 
+pub const NodeKey = struct { x: u64, a: u64 };
+pub const NodeCounts = std.AutoHashMap(NodeKey, u64);
+
 /// floor(sqrt(n)), exact for u64.
 pub fn isqrt(n: u64) u64 {
     if (n < 2) return n;
@@ -16,8 +19,15 @@ pub fn isqrt(n: u64) u64 {
     return x;
 }
 
-fn phi(x: u64, a: u64, primes: []const u64, pis: []const u64, c: *Counters) u64 {
+fn phi(x: u64, a: u64, primes: []const u64, pis: []const u64, c: *Counters, nc: *NodeCounts) !u64 {
     c.n += 1;
+
+    const xa = NodeKey{ .x = x, .a = a };
+    const result = try nc.getOrPut(xa);
+    if (!result.found_existing) {
+        result.value_ptr.* = 0;
+    }
+    result.value_ptr.* += 1;
 
     if (x == 0) {
         c.x0 += 1;
@@ -35,11 +45,20 @@ fn phi(x: u64, a: u64, primes: []const u64, pis: []const u64, c: *Counters) u64 
         return 1;
     }
 
+    // var b_lim = a;
+
+    if (x < p_a * p_a) {
+        assert(x > p_a);
+        const sqrt_x = isqrt(x);
+        const pi_sqrt_x = pis[sqrt_x];
+        return try phi(x, pi_sqrt_x, primes, pis, c, nc) - (a - pi_sqrt_x);
+    }
+
     var phi_val = x;
 
     for (1..(a + 1)) |b| {
         const p_b = primes[b];
-        phi_val -= phi(x / p_b, b - 1, primes, pis, c);
+        phi_val -= try phi(x / p_b, b - 1, primes, pis, c, nc);
     }
 
     return phi_val;
@@ -95,16 +114,18 @@ pub fn main(init: std.process.Init) !void {
     const a = primes.len - 1;
 
     var c = Counters{};
+    var nc = std.AutoHashMap(NodeKey, u64).init(gpa);
+    defer nc.deinit();
 
     const pis = try pisToY(gpa, y, primes);
     defer gpa.free(pis);
 
-    const phi_x_y = phi(x, a, primes, pis, &c);
+    const phi_x_y = try phi(x, a, primes, pis, &c, &nc);
 
     const n_f: f64 = @floatFromInt(c.n);
     const x_f: f64 = @floatFromInt(x);
     const ln_n: f64 = @log(n_f);
     const ln_x = @log(x_f);
 
-    std.debug.print("x: {d:>12} | y: {d:>6} ------ nodes/x: {d:>7.4} ln: {d:5.3} ------- phi(x,y): {d:>12} | {any}\n", .{ x, y, n_f / x_f, ln_n / ln_x, phi_x_y, c });
+    std.debug.print("x: {d:>12} | y: {d:>6} ------ nodes/x: {d:>7.4} ln: {d:5.3} ------- phi(x,y): {d:>12} | {any} | nc: {d:>12}\n", .{ x, y, n_f / x_f, ln_n / ln_x, phi_x_y, c, nc.count() });
 }
